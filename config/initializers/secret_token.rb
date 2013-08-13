5cb82dff7712b0ca2055a4af4447dbfc42dad315
Lawrence Luk <lluk@ucsd.edu>
2013-08-12 20:56:56 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShipprrLanding::Application.config.secret_key_base = '5f84e49a5d8d738bfd3e2192c8320bb1f1d6cf74d4ed349429129f3f71a5a11238c0241efea6f30f411f00dec7a12a3b3c154459ede1d39b3638a343e47294d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShipprrLanding::Application.config.session_store :cookie_store, key: '_shipprr_landing_session'
